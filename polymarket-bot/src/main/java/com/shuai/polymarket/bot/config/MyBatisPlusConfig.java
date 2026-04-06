7234f258debd6643da30a9a14829e0cb5e58b617
shuai <1195002311@qq.com>
2026-04-06 23:31:37 +0800

    init

---
+++ polymarket-bot/src/main/java/com/shuai/polymarket/bot/config/MyBatisPlusConfig.java
@@ -0,0 +1,20 @@
+package com.shuai.polymarket.bot.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.shuai.polymarket.bot.mapper")
+public class MyBatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        var interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
